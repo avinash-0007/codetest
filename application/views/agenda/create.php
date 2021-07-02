ce606e9cdef6119a391a33bcf3c7e569175e312f
avinashdchase <coolestavi@gmail.com>
2021-07-02 15:00:40 +0530

    add form menu added

--- application/views/agenda/create.php
+++ application/views/agenda/create.php
@@ -1,3 +1,15 @@
+<div class="row">
+    <div class="col-lg-12">           
+        <h2>Agenda CRUD           
+            <div class="pull-right">
+				<a class="btn btn-primary" href="<?php echo base_url('agenda');?>"> List Agenda</a>
+               <a class="btn btn-primary" href="<?php echo base_url('agenda/create');?>"> Create New Agenda</a>
+			   <a class="btn btn-primary" href="<?php echo base_url('agenda/import');?>">Import Agenda</a>
+			   <a class="btn btn-primary" href="<?php echo base_url('agenda/export');?>">Export Agenda</a>
+            </div>
+        </h2>
+     </div>
+</div>
 <h1 style="text-align:center;">Create Agenda</h1>
 <form method="post" name="agendaCreate" action="<?php echo base_url('agendaCreate');?>">
     <div class="row">
